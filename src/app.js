affadb78d4737291128f26c8e6e271a2acdade87
Junaid Sayyed <junaidsayyed778@gmail.com>
2026-03-04 15:42:53 +0530

    add updates

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser())
 app.use(morgan("dev"))
+app.get("/", (req, res) =>{
+    res.send("Ledger services is up and running!")
+})
 app.use("/api/auth", authRoute);
 app.use("/api/accounts", accountRoutes);
 app.use("/api/transactions", transactionRoutes)
